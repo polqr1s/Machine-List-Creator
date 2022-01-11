62153c740d6afde6deba3890724b09b3e16db5bd
polqr1s <36319624+polqr1s@users.noreply.github.com>
2022-01-11 12:06:37 -0800

    Add files via upload

--- MachineListCreator.py
+++ MachineListCreator.py
@@ -4,10 +4,10 @@
 def MachineList():
     machinePrefix = input("Enter the prefix of the machine's ID (ex. bccx, bcpsm, bcwsm)")
     
-    machineFirstSuffix = input("Enter the first suffix ID of the machine (ex. 1, 5, etc)")
+    machineFirstSuffix = input("Enter the first suffix ID of the machine range (ex. 1, 5, etc)")
     machineFirstSuffix = int(machineFirstSuffix)
     
-    machineLastSuffix = input("Enter the last suffix ID of the machine (ex. 80, 90, etc)")
+    machineLastSuffix = input("Enter the last suffix ID of the machine range (ex. 80, 90, etc)")
     machineLastSuffix = (int(machineLastSuffix) + 1)
     
     idNumberList = list(range(machineFirstSuffix, machineLastSuffix))
@@ -15,6 +15,8 @@ def MachineList():
     finalIdNumberList = [str(i).zfill(3) for i in idNumberList]
 
     print ([machinePrefix + str(i) for i in finalIdNumberList])
+    MachineList()
 
 
 MachineList()
+sleep()
